d3e45ec71464946de4e8e516438e8aa6c064b236
mahin-1 <96346554+mahin-1@users.noreply.github.com>
2022-07-10 18:27:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName("yes")[0].addEventListener("click",function(){
+  document.getElementsByClassName("options")[0].classList.remove("hideclass");
+});
+
+
+
+
+
+
+$('.carousel').carousel({
+  interval: 1000
+})
